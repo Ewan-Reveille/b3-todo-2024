32da5eaaedb2e2faf9b920b037e8f0de82807f5c
Eliot OMEN <eliotgiraud@gmail.com>
2024-10-14 16:09:35 +0200

    add title

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ if (isset($_GET['toggle_status'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-Do List avec PHP</title>
+    <title>To-Do List</title>
     <link href="./public/css/styles.css" rel="stylesheet">        
 </head>
 <body>
